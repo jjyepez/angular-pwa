86de2956d640a96ed6147301efcfbf595685baa1
jjyepez <jjyepez@gmail.com>
2018-06-03 23:59:52 -0430

    Avances - clase 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  // --- jjy
+  constructor(private swUpdate:SwUpdate){
+
+  }
+  ngOnInit(): void{
+  	if(this.swUpdate.isEnabled){
+  		this.swUpdate.available.subscribe(()=>{
+  			window.location.reload();
+  		});
+  	}
+  }
 }
